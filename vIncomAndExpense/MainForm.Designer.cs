c4756a2ac1c4826efe1c73d1acf141be497c0501
sefadonmez <sefadonmez@hotmail.com>
2024-05-18 00:55:46 +0300

    Add project files.

---
+++ vIncomAndExpense/MainForm.Designer.cs
@@ -0,0 +1,229 @@
+﻿using System.Windows.Forms;
+using System;
+
+namespace vIncomAndExpense
+{
+    partial class MainForm
+    {
+        private System.ComponentModel.IContainer components = null;
+        private TextBox txtAmount;
+        private TextBox txtDescription;
+        private RadioButton rbIncome;
+        private RadioButton rbExpense;
+        private Button btnAdd;
+        private DataGridView dataGridView;
+
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.txtAmount = new System.Windows.Forms.TextBox();
+            this.txtDescription = new System.Windows.Forms.TextBox();
+            this.rbIncome = new System.Windows.Forms.RadioButton();
+            this.rbExpense = new System.Windows.Forms.RadioButton();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.dataGridView = new System.Windows.Forms.DataGridView();
+            this.lblTotalIncome = new System.Windows.Forms.Label();
+            this.lblTotalExpense = new System.Windows.Forms.Label();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnSaveToPDF = new System.Windows.Forms.Button();
+            this.saveFileDialog = new System.Windows.Forms.SaveFileDialog();
+            this.btnSendEmail = new System.Windows.Forms.Button();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).BeginInit();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // txtAmount
+            // 
+            this.txtAmount.Location = new System.Drawing.Point(78, 8);
+            this.txtAmount.Name = "txtAmount";
+            this.txtAmount.Size = new System.Drawing.Size(200, 20);
+            this.txtAmount.TabIndex = 0;
+            this.txtAmount.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtAmount_KeyPress);
+            // 
+            // txtDescription
+            // 
+            this.txtDescription.Location = new System.Drawing.Point(78, 34);
+            this.txtDescription.Name = "txtDescription";
+            this.txtDescription.Size = new System.Drawing.Size(200, 20);
+            this.txtDescription.TabIndex = 1;
+            // 
+            // rbIncome
+            // 
+            this.rbIncome.AutoSize = true;
+            this.rbIncome.Location = new System.Drawing.Point(12, 64);
+            this.rbIncome.Name = "rbIncome";
+            this.rbIncome.Size = new System.Drawing.Size(60, 17);
+            this.rbIncome.TabIndex = 2;
+            this.rbIncome.TabStop = true;
+            this.rbIncome.Text = "Income";
+            this.rbIncome.UseVisualStyleBackColor = true;
+            // 
+            // rbExpense
+            // 
+            this.rbExpense.AutoSize = true;
+            this.rbExpense.Location = new System.Drawing.Point(78, 64);
+            this.rbExpense.Name = "rbExpense";
+            this.rbExpense.Size = new System.Drawing.Size(66, 17);
+            this.rbExpense.TabIndex = 3;
+            this.rbExpense.TabStop = true;
+            this.rbExpense.Text = "Expense";
+            this.rbExpense.UseVisualStyleBackColor = true;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(203, 61);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(75, 23);
+            this.btnAdd.TabIndex = 6;
+            this.btnAdd.Text = "Add";
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // dataGridView
+            // 
+            this.dataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView.Location = new System.Drawing.Point(12, 96);
+            this.dataGridView.Name = "dataGridView";
+            this.dataGridView.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dataGridView.Size = new System.Drawing.Size(547, 200);
+            this.dataGridView.TabIndex = 5;
+            // 
+            // lblTotalIncome
+            // 
+            this.lblTotalIncome.AutoSize = true;
+            this.lblTotalIncome.BackColor = System.Drawing.Color.LimeGreen;
+            this.lblTotalIncome.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblTotalIncome.Location = new System.Drawing.Point(12, 302);
+            this.lblTotalIncome.Name = "lblTotalIncome";
+            this.lblTotalIncome.Size = new System.Drawing.Size(57, 20);
+            this.lblTotalIncome.TabIndex = 7;
+            this.lblTotalIncome.Text = "label1";
+            // 
+            // lblTotalExpense
+            // 
+            this.lblTotalExpense.AutoSize = true;
+            this.lblTotalExpense.BackColor = System.Drawing.Color.Red;
+            this.lblTotalExpense.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblTotalExpense.Location = new System.Drawing.Point(12, 331);
+            this.lblTotalExpense.Name = "lblTotalExpense";
+            this.lblTotalExpense.Size = new System.Drawing.Size(57, 20);
+            this.lblTotalExpense.TabIndex = 8;
+            this.lblTotalExpense.Text = "label1";
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btnDelete.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnDelete.ForeColor = System.Drawing.Color.White;
+            this.btnDelete.Location = new System.Drawing.Point(477, 302);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(82, 46);
+            this.btnDelete.TabIndex = 9;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = false;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(43, 13);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "Amount";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(9, 38);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(60, 13);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "Description";
+            // 
+            // btnSaveToPDF
+            // 
+            this.btnSaveToPDF.Location = new System.Drawing.Point(471, 8);
+            this.btnSaveToPDF.Name = "btnSaveToPDF";
+            this.btnSaveToPDF.Size = new System.Drawing.Size(88, 34);
+            this.btnSaveToPDF.TabIndex = 12;
+            this.btnSaveToPDF.Text = "Save to PDF";
+            this.btnSaveToPDF.Click += new System.EventHandler(this.btnSaveToPDF_Click);
+            // 
+            // btnSendEmail
+            // 
+            this.btnSendEmail.Location = new System.Drawing.Point(471, 47);
+            this.btnSendEmail.Name = "btnSendEmail";
+            this.btnSendEmail.Size = new System.Drawing.Size(88, 34);
+            this.btnSendEmail.TabIndex = 13;
+            this.btnSendEmail.Text = "Send to Mail";
+            this.btnSendEmail.Click += new System.EventHandler(this.btnSendEmail_Click);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 368);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(564, 22);
+            this.statusStrip1.TabIndex = 14;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(118, 17);
+            this.toolStripStatusLabel1.Text = "toolStripStatusLabel1";
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // MainForm
+            // 
+            this.ClientSize = new System.Drawing.Size(564, 390);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.btnSendEmail);
+            this.Controls.Add(this.btnSaveToPDF);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.lblTotalExpense);
+            this.Controls.Add(this.lblTotalIncome);
+            this.Controls.Add(this.dataGridView);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.rbExpense);
+            this.Controls.Add(this.rbIncome);
+            this.Controls.Add(this.txtDescription);
+            this.Controls.Add(this.txtAmount);
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Income-Expense Tracker";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).EndInit();
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        private Label lblTotalIncome;
+        private Label lblTotalExpense;
+        private Button btnDelete;
+        private Label label1;
+        private Label label2;
+        private Button btnSaveToPDF;
+        private SaveFileDialog saveFileDialog;
+        private Button btnSendEmail;
+        private StatusStrip statusStrip1;
+        private ToolStripStatusLabel toolStripStatusLabel1;
+        private Timer timer1;
+    }
+
+}
+
---
+++ vIncomAndExpense/MainForm.cs
@@ -0,0 +1,350 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Data.SQLite;
+using System.Drawing.Printing;
+using System.IO;
+using System.Xml.Linq;
+using iTextSharp.text;
+using iTextSharp.text.pdf;
+using System.Net.Mail;
+using System.Net;
+using System.Xml;
+namespace vIncomAndExpense
+{
+    public partial class MainForm : Form
+    {
+        private const string ConnectionString = "Data Source=Database/data.db;Version=3;";
+        public MainForm()
+        {
+            InitializeComponent();
+            InitializeDatabase();
+            LoadTransactions();
+            UpdateTotals();
+            txtAmount.KeyPress += new KeyPressEventHandler(txtAmount_KeyPress); // txtAmount için KeyPress olayına işleyici ekleme
+            timer1.Interval = 1000; // Zamanlayıcı her bir saniyede bir etkinleştirilir
+            timer1.Tick += timer1_Tick; // Zamanlayıcı etkin olduğunda çağrılacak olay
+            timer1.Start(); // Zamanlayıcıyı başlat
+        }
+
+        public class SmtpSettings
+        {
+            public string Host { get; set; }
+            public int Port { get; set; }
+            public string Username { get; set; }
+            public string Password { get; set; }
+            public string FromAddress { get; set; }
+            public string ToAddress { get; set; }
+            public string Subject { get; set; }
+        }
+        public static void InitializeDatabase()
+        {
+            using (var connection = new SQLiteConnection(ConnectionString))
+            {
+                connection.Open();
+
+                string createTableQuery = @"
+                CREATE TABLE IF NOT EXISTS Transactions (
+                    Id INTEGER PRIMARY KEY AUTOINCREMENT,
+                    Type TEXT NOT NULL,
+                    Amount REAL NOT NULL,
+                    Description TEXT,
+                    Date TEXT NOT NULL
+                );";
+
+                using (var command = new SQLiteCommand(createTableQuery, connection))
+                {
+                    command.ExecuteNonQuery();
+                }
+            }
+        }
+
+        private void UpdateTotals()
+        {
+            using (var connection = new SQLiteConnection(ConnectionString))
+            {
+                connection.Open();
+
+                // Total Income
+                string incomeQuery = "SELECT SUM(Amount) FROM Transactions WHERE Type='Income'";
+                using (var incomeCommand = new SQLiteCommand(incomeQuery, connection))
+                {
+                    var result = incomeCommand.ExecuteScalar();
+                    lblTotalIncome.Text = "Total Expense: " + (result != DBNull.Value ? Convert.ToDouble(result).ToString("C") : "0");
+                }
+
+                // Total Expense
+                string expenseQuery = "SELECT SUM(Amount) FROM Transactions WHERE Type='Expense'";
+                using (var expenseCommand = new SQLiteCommand(expenseQuery, connection))
+                {
+                    var result = expenseCommand.ExecuteScalar();
+                    lblTotalExpense.Text = "Total Income: " + (result != DBNull.Value ? Convert.ToDouble(result).ToString("C") : "0");
+                }
+            }
+        }
+
+        private void LoadTransactions()
+        {
+            using (var connection = new SQLiteConnection(ConnectionString))
+            {
+                connection.Open();
+                string selectQuery = "SELECT * FROM Transactions";
+
+                using (var adapter = new SQLiteDataAdapter(selectQuery, connection))
+                {
+                    DataTable dataTable = new DataTable();
+                    adapter.Fill(dataTable);
+                    dataGridView.DataSource = dataTable;
+                }
+            }
+        }
+
+        private void ClearForm()
+        {
+            txtAmount.Clear();
+            txtDescription.Clear();
+            rbIncome.Checked = true;
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            try
+            {
+
+                string type = rbIncome.Checked ? "Income" : "Expense";
+                double amount = double.Parse(txtAmount.Text);
+                string description = txtDescription.Text;
+                string date = DateTime.Now.ToString("yyyy-MM-dd");
+
+                using (var connection = new SQLiteConnection(ConnectionString))
+                {
+                    connection.Open();
+                    string insertQuery = "INSERT INTO Transactions (Type, Amount, Description, Date) VALUES (@type, @amount, @description, @date)";
+
+                    using (var command = new SQLiteCommand(insertQuery, connection))
+                    {
+                        command.Parameters.AddWithValue("@type", type);
+                        command.Parameters.AddWithValue("@amount", amount);
+                        command.Parameters.AddWithValue("@description", description);
+                        command.Parameters.AddWithValue("@date", date);
+
+                        command.ExecuteNonQuery();
+                    }
+                }
+
+                LoadTransactions();
+                UpdateTotals();
+                ClearForm();
+
+            }
+            catch { }
+
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            if (dataGridView.SelectedRows.Count > 0)
+            {
+                int selectedRowIndex = dataGridView.SelectedRows[0].Index;
+                int id = Convert.ToInt32(dataGridView.Rows[selectedRowIndex].Cells["Id"].Value);
+
+                using (var connection = new SQLiteConnection(ConnectionString))
+                {
+                    connection.Open();
+                    string deleteQuery = "DELETE FROM Transactions WHERE Id=@id";
+
+                    using (var command = new SQLiteCommand(deleteQuery, connection))
+                    {
+                        command.Parameters.AddWithValue("@id", id);
+                        command.ExecuteNonQuery();
+                    }
+                }
+
+                LoadTransactions();
+                UpdateTotals();
+            }
+            else
+            {
+                MessageBox.Show("Please Select Datarow.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+        private string pdfFileName; // PDF dosyasının adını saklamak için değişken
+
+        private void btnSaveToPDF_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Filter = "PDF Files|*.pdf";
+            saveFileDialog.Title = "Save as PDF";
+            saveFileDialog.FileName = "IncomeExpenseReport.pdf";
+
+            if (saveFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                pdfFileName = saveFileDialog.FileName;
+                using (var connection = new SQLiteConnection(ConnectionString))
+                {
+                    connection.Open();
+                    string selectQuery = "SELECT * FROM Transactions";
+
+                    using (var adapter = new SQLiteDataAdapter(selectQuery, connection))
+                    {
+                        DataTable dataTable = new DataTable();
+                        adapter.Fill(dataTable);
+
+                        using (FileStream stream = new FileStream(saveFileDialog.FileName, FileMode.Create))
+                        {
+                            Document pdfDoc = new Document(PageSize.A4);
+                            PdfWriter.GetInstance(pdfDoc, stream);
+                            pdfDoc.Open();
+
+                            // Title
+                            var titleFont = FontFactory.GetFont("Arial", "10", Font.Bold);
+                            Paragraph title = new Paragraph("Income and Expense Report", titleFont);
+                            title.Alignment = Element.ALIGN_CENTER;
+                            pdfDoc.Add(title);
+
+                            pdfDoc.Add(new Paragraph(" ")); // Boşluk
+
+                            PdfPTable table = new PdfPTable(dataTable.Columns.Count);
+                            table.WidthPercentage = 100;
+
+                            // Header
+                            var headerFont = FontFactory.GetFont("Arial", "10", Font.Bold);
+                            foreach (DataColumn column in dataTable.Columns)
+                            {
+                                PdfPCell cell = new PdfPCell(new Phrase(column.ColumnName, headerFont));
+                                cell.BackgroundColor = BaseColor.LIGHT_GRAY;
+                                table.AddCell(cell);
+                            }
+
+                            // Data
+                            var cellFont = FontFactory.GetFont("Arial", 12);
+                            foreach (DataRow row in dataTable.Rows)
+                            {
+                                foreach (var cell in row.ItemArray)
+                                {
+                                    table.AddCell(new Phrase(cell.ToString(), cellFont));
+                                }
+                            }
+
+                            pdfDoc.Add(table);
+
+                            // Totals
+                            pdfDoc.Add(new Paragraph(" ")); // Boşluk
+                            double totalIncome = 0;
+                            double totalExpense = 0;
+
+                            foreach (DataRow row in dataTable.Rows)
+                            {
+                                if (row["Type"].ToString() == "Income")
+                                {
+                                    totalIncome += Convert.ToDouble(row["Amount"]);
+                                }
+                                else if (row["Type"].ToString() == "Expense")
+                                {
+                                    totalExpense += Convert.ToDouble(row["Amount"]);
+                                }
+                            }
+
+                            var totalFont = FontFactory.GetFont("Arial", "12", Font.Bold);
+                            pdfDoc.Add(new Paragraph($"Total Income: {totalIncome:C}", totalFont));
+                            pdfDoc.Add(new Paragraph($"Total Expense: {totalExpense:C}", totalFont));
+                            pdfDoc.Add(new Paragraph($"Net Income: {(totalIncome - totalExpense):C}", totalFont));
+
+                            pdfDoc.Close();
+                            stream.Close();
+                        }
+                    }
+                }
+            }
+        }
+
+        private void btnSendEmail_Click(object sender, EventArgs e)
+        {
+            SmtpSettings smtpSettings = LoadSmtpSettings("smtpSettings.xml");
+
+            if (smtpSettings == null)
+            {
+                MessageBox.Show("SMTP ayarları yüklenemedi.", "Hata", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            if (string.IsNullOrEmpty(pdfFileName))
+            {
+                MessageBox.Show("Lütfen önce bir PDF dosyası oluşturun ve kaydedin.", "Uyarı", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            try
+            {
+                MailMessage mail = new MailMessage(smtpSettings.FromAddress, smtpSettings.ToAddress);
+                mail.Subject = smtpSettings.Subject;
+                mail.Body = "Gelir ve gider raporu ektedir.";
+
+                Attachment attachment = new Attachment(pdfFileName);
+                mail.Attachments.Add(attachment);
+
+                SmtpClient smtpClient = new SmtpClient(smtpSettings.Host, smtpSettings.Port);
+                smtpClient.UseDefaultCredentials = false;
+                smtpClient.Credentials = new NetworkCredential(smtpSettings.Username, smtpSettings.Password);
+                smtpClient.EnableSsl = true; // SSL/TLS şifreleme kullanılacaksa true olarak ayarlanmalı
+
+                smtpClient.Send(mail);
+
+                MessageBox.Show("Mail başarıyla gönderildi.", "Başarılı", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Mail gönderilirken bir hata oluştu: " + ex.Message, "Hata", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void txtAmount_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            // Yalnızca rakamlar ve geri silme tuşu izin verilir
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar))
+            {
+                e.Handled = true;
+            }
+        }
+
+        private SmtpSettings LoadSmtpSettings(string filePath)
+        {
+            SmtpSettings settings = new SmtpSettings();
+
+            try
+            {
+                XmlDocument doc = new XmlDocument();
+                doc.Load(filePath);
+
+                settings.Host = doc.SelectSingleNode("/SmtpSettings/Host").InnerText;
+                settings.Port = int.Parse(doc.SelectSingleNode("/SmtpSettings/Port").InnerText);
+                settings.Username = doc.SelectSingleNode("/SmtpSettings/Username").InnerText;
+                settings.Password = doc.SelectSingleNode("/SmtpSettings/Password").InnerText;
+                settings.FromAddress = doc.SelectSingleNode("/SmtpSettings/FromAddress").InnerText;
+                settings.ToAddress = doc.SelectSingleNode("/SmtpSettings/ToAddress").InnerText;
+                settings.Subject = doc.SelectSingleNode("/SmtpSettings/Subject").InnerText;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("SMTP ayarları yüklenirken bir hata oluştu: " + ex.Message, "Hata", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+            return settings;
+        }
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            toolStripStatusLabel1.Text = DateTime.Now.ToString("dd/MM/yyyy HH:mm:ss"); // Güncel tarih ve saat bilgisini toolStripStatusLabel1 içine yazdır
+
+        }
+    }
+}
